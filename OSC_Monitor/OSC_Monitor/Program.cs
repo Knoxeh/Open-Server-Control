f42521af880a8e7ea0918f424fb17961bca8d4d7
Knoxeh <nolan@knoxeh.com>
2013-07-28 16:32:42 -0400

    OSC_Monitor Base
    +Add base OSC_Montitor (C# Server Monitor for Windows)

---
+++ OSC_Monitor/OSC_Monitor/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OSC_Monitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
